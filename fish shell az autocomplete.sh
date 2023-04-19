ea4a05079f1ac11ea0fa808ed251e4c9cf2cc5dc
HeeLoco <66466597+HeeLoco@users.noreply.github.com>
2023-04-19 15:36:48 +0200

    Create fish shell az autocomplete.sh

---
+++ fish shell az autocomplete.sh
@@ -0,0 +1,7 @@
+pip install argcomplete
+sudo activate-global-python-argcomplete
+register-python-argcomplete --shell fish az > ~/.config/fish/completions/az.fish
+
+#links
+#https://github.com/Azure/azure-cli/issues/1031
+#https://github.com/kislyuk/argcomplete
